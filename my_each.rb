f50769cbc5a6edd4ee8cb66b4decc0c3839685de
pekellogg <pekellogg@hotmail.com>
2021-04-21 15:16:46 -0700

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,12 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(ints_array)
+  if block_given?
+  i = 0
+  while i < ints_array.length
+      yield ints_array[i]
+      i += 1
+  end
+  ints_array
+  else
+    return "No block given to yield for my_each"
+  end
 end
